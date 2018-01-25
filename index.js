7dc78c8ff45a7173343f68bc9d4dc43b6a334b4a
vale17accidentidellastoria <pingupucca@gmail.com>
2018-01-25 06:43:47 +0100

    gets resulting area from calling the function getArea() in area.js

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const area = require('./area');
+const getArea = require('./area');
 
 const app = express();
 app.use(bodyParser.json());
-
 
 app.get('/:inputs', function(req,res){
 
     var lato1 = parseInt(req.query.side1);
     var lato2 = parseInt(req.query.side2);
-    var result = lato1*lato2;
-    //res.send(result.toString());
+    var result = getArea([lato1,lato2]);
     var json = JSON.stringify({
         area: result
     });
