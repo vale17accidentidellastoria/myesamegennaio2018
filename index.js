276ae8d0bbb4371bcc6e6acce1bba5a98d7238c4
vale17accidentidellastoria <pingupucca@gmail.com>
2018-07-09 10:05:56 +0200

    fix json in index.js

--- index.js
+++ index.js
@@ -10,10 +10,19 @@ app.get('/:inputs', function(req,res){
     var lato1 = parseInt(req.query.side1);
     var lato2 = parseInt(req.query.side2);
     var result = getArea([lato1,lato2]);
-    var json = JSON.stringify({
-        area: result
-    });
-    res.end(json);
+	 var json;
+	 if(result===-1) {
+    	json = JSON.stringify({
+        	"area": result
+    	});
+		res.status(400).send(json);
+ 	} else {
+    	json = JSON.stringify({
+        	"area": result
+    	});
+		res.send(json);
+ 	}
+   
 });
 
 app.listen((process.env.PORT || 3000));
